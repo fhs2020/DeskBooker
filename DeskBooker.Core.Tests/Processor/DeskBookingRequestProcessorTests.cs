42feb1dec124fe01ae3587c93b36e81e4d73502f
fhs2020 <flavioh007@gmail.com>
2021-12-30 12:09:58 -0300

    Add project files.

---
+++ DeskBooker.Core.Tests/Processor/DeskBookingRequestProcessorTests.cs
@@ -0,0 +1,52 @@
+﻿using DeskBooker.Core.Domain;
+
+using System;
+
+using Xunit;
+
+namespace DeskBooker.Core.Processor
+{
+    public class DeskBookingRequestProcessorTests
+    {
+        private readonly DeskBookingRequestProcessor _processor;
+
+        public DeskBookingRequestProcessorTests()
+        {
+            _processor = new DeskBookingRequestProcessor();
+        }
+
+
+        [Fact]
+        public void ShouldReturnDeskBookingResultWithRequestValues()
+        {
+            // Arrange
+            var request = new DeskBookingRequest
+            {
+                FirstName = "Thomas",
+                LastName = "Huber",
+                Email = "thomas@thoamsclaudiushuber.com",
+                Date = new DateTime(2020, 01, 28)
+            };
+
+
+            // Act
+            DeskbookingResult result = _processor.BookDesk(request);
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Equal(request.FirstName, result.FirstName);
+            Assert.Equal(request.LastName, result.LastName);
+            Assert.Equal(request.Email, result.Email);
+            Assert.Equal(request.Date, result.Date);
+        }
+
+        [Fact]
+        public void ShouldThrowExceptionIfRequestIsNull()
+        {
+            var exception = Assert.Throws<ArgumentNullException>(() => _processor.BookDesk(null));
+
+            Assert.Equal("request", exception.ParamName);
+
+        }
+    }
+}
---
+++ DeskBooker.Core/Processor/DeskBookingRequestProcessor.cs
@@ -0,0 +1,26 @@
+﻿using DeskBooker.Core.Domain;
+
+namespace DeskBooker.Core.Processor
+{
+    public class DeskBookingRequestProcessor
+    {
+        public DeskBookingRequestProcessor()
+        {
+        }
+
+        public DeskbookingResult BookDesk(DeskBookingRequest request)
+        {
+            if (request == null)
+                throw new ArgumentNullException(nameof(request));
+
+
+            return new DeskbookingResult
+            {
+                FirstName = request.FirstName,
+                LastName = request.LastName,
+                Email = request.Email,
+                Date = request.Date,
+            };
+        }
+    }
+}
